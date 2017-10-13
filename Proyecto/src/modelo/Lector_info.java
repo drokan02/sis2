f737a12dbd6c682b37f5f9ace587e6cd13644a22
isaacsoto <isaacsotomar@gmail.com>
2017-10-13 02:15:55 -0400

    Create Lector_info.java

---
+++ Proyecto/src/modelo/Lector_info.java
@@ -0,0 +1,28 @@
+
+package modelo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class Lector_info {
+    public static String leertxt(String direccion){
+      String text="";
+    try{
+        BufferedReader bf = new BufferedReader(new FileReader(direccion));
+        String aux="";
+        String read;
+        while((read = bf.readLine()) !=null){
+            aux =aux+read;
+        }
+        text = aux;
+    }catch(Exception e){
+        System.err.println("Archivo no encontrado");
+    }    
+    return  text;
+    }
+    public static void main(String [] args){
+        leertxt("src\\modelo\\aspectos_basicos.txt");
+        
+    }
+}
