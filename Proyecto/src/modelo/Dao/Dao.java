1f3753dd4e48df6b2869984f82c4f60a7fbb1727
isaacsoto <isaacsotomar@gmail.com>
2017-10-13 02:38:01 -0400

    Estandares Corregidos

--- Proyecto/src/modelo/Dao/Dao.java
+++ Proyecto/src/modelo/Dao/Dao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo.Dao;
 
 import java.util.List;
@@ -13,9 +8,9 @@ import java.util.List;
  */
 public interface Dao <T,K,G> {
     
-   public void insertar(T a);
-   public void modificar(T a);
-   public void eliminar(T a);
-   public List<T> listar(); 
-   public T buscar(G id);
+  public void insertar(T a);
+  public void modificar(T a);
+  public void eliminar(T a);
+  public List<T> listar(); 
+  public T buscar(G id);
 }
--- Proyecto/src/modelo/VO/Documento.java
+++ Proyecto/src/modelo/VO/Documento.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo.VO;
 
 import java.awt.Desktop;
@@ -20,43 +15,43 @@ import sun.awt.DesktopBrowse;
  * @author Raul
  */
 public class Documento {
-    private File archivo;
-    
-    public Documento(){
-        fileChooser();
-    }
-    
-    public Documento(File archivo){
-    this.archivo = archivo;
-    }
-    
-    private void fileChooser(){
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Buscar Documento");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("All Document", "*.*"),
-                new FileChooser.ExtensionFilter("PDF", "*.pdf"),
-                new FileChooser.ExtensionFilter("DOCX", "*.doc")
-        );
-        Stage stage = new Stage();
-        archivo = fileChooser.showOpenDialog(stage);       
-    }
-    public File getArchivo() {
-        return archivo;
-    }
+  private File archivo;
 
-    public void setArchivo(File archivo) {
-        this.archivo = archivo;
+  public Documento(){
+    fileChooser();
+  }
+
+  public Documento(File archivo){
+    this.archivo = archivo;
+  }
+
+  private void fileChooser(){
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Buscar Documento");
+    fileChooser.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("All Document", "*.*"),
+            new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+            new FileChooser.ExtensionFilter("DOCX", "*.doc")
+    );
+    Stage stage = new Stage();
+    archivo = fileChooser.showOpenDialog(stage);       
+  }
+  public File getArchivo() {
+    return archivo;
+  }
+
+  public void setArchivo(File archivo) {
+    this.archivo = archivo;
+  }
+
+  public boolean validarDocumento(String[] extenciones){
+    boolean res = true;
+    int i = 0;
+    while(i < extenciones.length && res == true){
+        if(!archivo.getName().endsWith(extenciones[i]))
+            res = false;
+        i++;
     }
-    
-    public boolean validarDocumento(String[] extenciones){
-        boolean res = true;
-        int i = 0;
-        while(i < extenciones.length && res == true){
-            if(!archivo.getName().endsWith(extenciones[i]))
-                res = false;
-            i++;
-        }
-        return res;
-    }
+    return res;
+  }
 }
