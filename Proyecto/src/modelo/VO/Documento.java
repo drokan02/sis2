ffd0d27f0b7925a4530a3e5b460fba6c1ba395dd
drokan2 <dejota423@gmail.com>
2017-10-12 21:04:35 -0400

    conexion de la BD con el proyecto

--- Proyecto/src/modelo/VO/Documento.java
+++ Proyecto/src/modelo/VO/Documento.java
@@ -5,9 +5,15 @@
  */
 package modelo.VO;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import sun.awt.DesktopBrowse;
 
 /**
  *
@@ -19,20 +25,21 @@ public class Documento {
     public Documento(){
         fileChooser();
     }
+    
     public Documento(File archivo){
     this.archivo = archivo;
     }
+    
     private void fileChooser(){
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Buscar Documento");
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("All Document", "*.*"),
                 new FileChooser.ExtensionFilter("PDF", "*.pdf"),
-                new FileChooser.ExtensionFilter("DOC", "*.doc")
+                new FileChooser.ExtensionFilter("DOCX", "*.doc")
         );
         Stage stage = new Stage();
-        archivo = fileChooser.showOpenDialog(stage);
-        
+        archivo = fileChooser.showOpenDialog(stage);       
     }
     public File getArchivo() {
         return archivo;
@@ -42,7 +49,7 @@ public class Documento {
         this.archivo = archivo;
     }
     
-    public boolean validarDocumento(String [] extenciones){
+    public boolean validarDocumento(String[] extenciones){
         boolean res = true;
         int i = 0;
         while(i < extenciones.length && res == true){
